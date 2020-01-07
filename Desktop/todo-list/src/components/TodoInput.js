306c62b882b08261b669b76545d66d8811f7b394
destiny <destiny-mac@destinys-MacBook-Pro.local>
2020-01-07 15:16:34 +0100

    second commit

--- Desktop/todo-list/src/components/TodoInput.js
+++ Desktop/todo-list/src/components/TodoInput.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 export default class TodoInput extends Component {
   render() {
+    const { item, handleChange, handleSubmit, editItem } = this.props;
     return <h1>hello from todo input</h1>;
   }
 }
